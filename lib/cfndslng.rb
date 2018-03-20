e541282954b33e61f267a99370990054e079821e
Justin Rowles <justin.rowles.casual@guardian.co.uk>
2018-03-20 09:43:13 +0000

    Add ruby wiring

---
+++ lib/cfndslng.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.push File.expand_path(__dir__) unless $LOAD_PATH.include?(File.expand_path(__dir__))
+
+require 'cfndsl'
+
+require 'cfndslng/ruby/add'
+require 'cfndslng/templates'
+
